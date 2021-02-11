10998e8f56704ba8e67e308dcd1bbc7f7c085752
lena <llena0964@gmail.com>
2021-02-11 14:25:52 +0200

    text

--- src/components/trip-lists/index.js
+++ src/components/trip-lists/index.js
@@ -135,7 +135,7 @@ TripLists.defaultProps = {
             id: 3,
             number: '03.',
             title: 'Chill Adventure3',
-            desc: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
+            desc: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from BC',
             image: 'trip-s3-min.png'
         },
         {
